9cd8e047e78175a05efe044293a56f0619b5c514
YanaKrukovska <jana.krua@gmail.com>
2021-01-12 16:31:59 +0200

    - Practice task: switching between pages on one server

--- ua.edu.distedu.ykrukovska/first-practice/server.js
+++ ua.edu.distedu.ykrukovska/first-practice/server.js
@@ -6,6 +6,10 @@ server.listen(8888);
 console.log('Server is running on port 8888');
 
 server.use(express.static(__dirname));
-server.get('/', function (req, res) {
+server.get('/p1', function (req, res) {
     res.sendFile(__dirname + "/firstpage.html");
+});
+
+server.get('/p2', function (req, res) {
+    res.sendFile(__dirname + "/secondpage.html");
 });
