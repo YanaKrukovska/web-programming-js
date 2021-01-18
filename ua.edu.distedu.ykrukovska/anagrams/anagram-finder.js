e896a6cd38d4858e633082b05cbc18b2e208b0a0
YanaKrukovska <jana.krua@gmail.com>
2021-01-18 17:33:06 +0200

    - Implemented search of anagrams in a file

---
+++ ua.edu.distedu.ykrukovska/anagrams/anagram-finder.js
@@ -0,0 +1,37 @@
+function readFile(input) {
+    const fs = require("fs");
+    const text = fs.readFileSync(input).toString('utf-8');
+    return text.split(/\s+/);
+}
+
+function sortLetters(word) {
+    return word.split('').sort().join('');
+}
+
+function writeAnagrams(anagrams) {
+    for (let key in anagrams) {
+        if (anagrams.hasOwnProperty(key) && anagrams[key].length > 1) {
+            let values = anagrams[key];
+            let res = "";
+            for (let i = 0; i < values.length; i++) {
+                res += i === values.length - 1 ? values[i] : values[i] + " - ";
+            }
+            console.log(res);
+        }
+    }
+}
+
+function findAnagrams(fileName) {
+    let words = readFile(__dirname + fileName);
+    let anagrams = {}
+
+    for (let word of words) {
+        let sortedWord = sortLetters(word);
+        anagrams[sortedWord] = anagrams[sortedWord] || []
+        anagrams[sortedWord].push(word)
+    }
+
+    writeAnagrams(anagrams);
+}
+
+findAnagrams("/anagrams.txt");
